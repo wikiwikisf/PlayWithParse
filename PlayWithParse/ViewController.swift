8212fbcf13eeb7f690a20c66a0f2a1c1311cd032
Vicki Grospe <vickichun@gmail.com>
2015-10-16 22:36:43 -0700

    Customize title for login page

--- PlayWithParse/ViewController.swift
+++ PlayWithParse/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  PlayWithParse
 //
@@ -31,7 +31,21 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
 
       loginViewController.emailAsUsername = true
       
+      let loginLogoTitle = UILabel()
+      loginLogoTitle.text = "Sway"
+      loginLogoTitle.font = UIFont.boldSystemFontOfSize(18.0)
+      loginViewController.logInView?.logo = loginLogoTitle
+
       loginViewController.signUpController?.delegate = self
+      
+      let signUpLogoTitle = UILabel()
+      signUpLogoTitle.text = "Sway"
+      signUpLogoTitle.font = UIFont.boldSystemFontOfSize(18.0)
+      
+      loginViewController.signUpController?.signUpView?.logo = signUpLogoTitle
+      loginViewController.signUpController?.delegate = self
+      
+      
       self.presentViewController(loginViewController, animated: false, completion: nil)
     } else {
       self.performSegueWithIdentifier("loginSegue", sender: self)
@@ -44,10 +58,47 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpV
     self.performSegueWithIdentifier("loginSegue", sender: self)
   }
   
+  func logInViewController(logInController: PFLogInViewController, shouldBeginLogInWithUsername username: String, password: String) -> Bool {
+    
+    if (!username.isEmpty && !password.isEmpty) {
+      return true
+    } else {
+      print("username or password empty")
+      return false
+    }
+  }
+  
+  func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
+    print("Failed to login \(error?.description)")
+  }
+  
   //MARK: PFSignUpViewControllerDelegate
   func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
     self.dismissViewControllerAnimated(true, completion: nil)
     self.performSegueWithIdentifier("loginSegue", sender: self)
+  }
+  
+  func signUpViewController(signUpController: PFSignUpViewController, shouldBeginSignUp info: [NSObject : AnyObject]) -> Bool {
+    var isInfoComplete : Bool = true
+    // TODO
+    /*for (key, value) in info {
+      let fieldValue: AnyObject? = value
+      if (fieldValue?.length < 1) {
+        isInfoComplete = false
+        break;
+      } else if signUpController.signUpView?.usernameField!.text! ==  nil {
+        isInfoComplete = false
+        break;
+      } else if signUpController.signUpView?.passwordField!.text! == nil {
+        isInfoComplete = false
+        break;
+      }
+    }*/
+    return isInfoComplete
+  }
+  
+  func signUpViewController(signUpController: PFSignUpViewController, didFailToSignUpWithError error: NSError?) {
+    print("Failed to signup \(error?.description)")
   }
   
   
